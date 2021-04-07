ea3066f9bed67dd276751658acc22990bde6a2d4
samatoki <81175009+samatoki@users.noreply.github.com>
2021-04-07 09:47:21 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("This is a line I'm uploading to GitHub");
+        // "staging" a file is called "commiting"
+        // "Uploading" the file to the repo is called "push"
+    }
+}
